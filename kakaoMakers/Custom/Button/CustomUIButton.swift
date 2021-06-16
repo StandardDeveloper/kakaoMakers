8fba8a4fc29311b5417c73e1e3fa92d64a368526
minwook.lee <minwook-lee@naver.com>
2021-06-16 23:35:25 +0900

    오픈예정 페이지 title제외 완성

---
+++ kakaoMakers/Custom/Button/CustomUIButton.swift
@@ -0,0 +1,22 @@
+//
+//  CustomUIButton.swift
+//  kakaoMakers
+//
+//  Created by 이민욱 on 2021/06/16.
+//
+
+import UIKit
+
+class CustomUIButton: UIButton {
+
+
+    required init(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)!
+        self.layer.cornerRadius = 5.0;
+        self.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        self.tintColor = UIColor.lightGray
+        
+    }
+
+   
+}
---
+++ kakaoMakers/Custom/TableViewCell/CommingSoonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommingSoonTableViewCell.swift
+//  kakaoMakers
+//
+//  Created by 이민욱 on 2021/06/16.
+//
+
+import UIKit
+
+class CommingSoonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
