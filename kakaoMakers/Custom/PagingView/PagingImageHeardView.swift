651efd30c9051c07116e95d1a483d7e44dbc6834
minwook.lee <minwook-lee@naver.com>
2021-06-24 23:50:59 +0900

    상세페이지 작업 진행중

---
+++ kakaoMakers/Custom/PagingView/PagingImageHeardView.swift
@@ -0,0 +1,20 @@
+//
+//  PagingImageHeardView.swift
+//  kakaoMakers
+//
+//  Created by 이민욱 on 2021/06/24.
+//
+
+import UIKit
+
+class PagingImageHeardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
