5c3711d592da2a800e8608bda8f0f4d05083168a
minwook.lee <minwook-lee@naver.com>
2021-06-25 23:12:58 +0900

    검색페이지 구현

---
+++ kakaoMakers/Custom/CollectionViewCell/SearchCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  SearchCollectionReusableView.swift
+//  kakaoMakers
+//
+//  Created by 이민욱 on 2021/06/25.
+//
+
+import UIKit
+
+class SearchCollectionReusableView: UICollectionReusableView {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
--- kakaoMakers/UI/SearchViewController.swift
+++ kakaoMakers/UI/SearchViewController.swift
@@ -7,23 +7,102 @@
 
 import UIKit
 
+
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchCollectionView: UICollectionView!
+    
+    var networkProvider = MovieNetworkManager()
+    private var movieListVM: MovieNowPlaying!
+    
+    let lineSpacing: CGFloat = 5
+    let inset: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        searchCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        searchCollectionView.delegate = self
+        searchCollectionView.dataSource = self
+   
+        let nibName = UINib(nibName: "CategoryCollectionViewCell", bundle: nil)
+        searchCollectionView.register(nibName, forCellWithReuseIdentifier: "categoryCollectionViewCell")
+        
+        getMovieData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getMovieData() {
+        
+        networkProvider.getMovies(target: .nowPlaying) { results in
+            
+            self.movieListVM = MovieNowPlaying(nowPlaying: results)
+                
+            OperationQueue.main.addOperation {
+                self.searchCollectionView.reloadData()
+            }
+        }
     }
-    */
-
 }
+
+extension SearchViewController: UICollectionViewDataSource {
+    
+//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+//        switch kind {
+//        case UICollectionView.elementKindSectionHeader:
+//            let headerView = searchCollectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "searchResuableView", for: indexPath) as! SearchCollectionReusableView
+//            return headerView
+//        default:
+//            assert(false)
+//        }
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+//        let width: CGFloat = collectionView.frame.width
+//        let height: CGFloat = 10
+//        return CGSize(width: width, height: height)
+//    }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return self.movieListVM == nil ? 0 : self.movieListVM.numberOfSections
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.movieListVM.numberOfRowInSections(section)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = searchCollectionView.dequeueReusableCell(withReuseIdentifier: "categoryCollectionViewCell", for: indexPath) as! CategoryCollectionViewCell
+        let movieVM = self.movieListVM.movieAtIndex(indexPath.section, index: indexPath.row)
+        let imagePath = "https://image.tmdb.org/t/p/w500\(movieVM.poster_path!)"
+        cell.posterImageView.kf.setImage(with: URL(string: imagePath))
+        cell.titleLabel.text = movieVM.title
+        cell.priceLabel.text = movieVM.release_data
+        return cell
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    
+    //상하간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return inset
+    }
+    //좌우간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return lineSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        ///  2등분하여 배치, 옆 간격이 1이므로 1을 빼줌
+        let width = collectionView.safeAreaLayoutGuide.layoutFrame.width / 2 - inset - lineSpacing - 10
+        let size = CGSize(width: width, height: 280)
+        return size
+    }
+}
+
+
