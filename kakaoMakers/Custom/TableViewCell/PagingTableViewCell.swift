bef5bad2c1029f9cb8c73ea56476403ff938e3de
minwook.lee <minwook-lee@naver.com>
2021-07-03 00:53:34 +0900

    PagingControl 기능 추가

--- kakaoMakers/Custom/TableViewCell/PagingTableViewCell.swift
+++ kakaoMakers/Custom/TableViewCell/PagingTableViewCell.swift
@@ -9,16 +9,93 @@ import UIKit
 
 class PagingTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var pagingCollectionView: UICollectionView!
+    
+    var networkProvider = MovieNetworkManager()
+    var movieImages = [MovieImage]()
+    
+    var setMovieID : ((Int) -> (Int))?
+    
+    var movieID : Int = 508943
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        pagingCollectionView.dataSource = self
+        pagingCollectionView.delegate = self
+        pagingCollectionView.register(UINib(nibName: "PagingImageCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "pagingImageCollectionCell")
+        pagingCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        getMovieImages()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        pageControl.numberOfPages = movieImages.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.gray
+        pageControl.currentPageIndicatorTintColor = UIColor.white
+    }
+    
+    func getMovieImages() {
+        networkProvider.getMovieImages(movieID: movieID) { results in
+
+            self.movieImages = results
+            
+            OperationQueue.main.addOperation {
+                self.pagingCollectionView.reloadData()
+            }
+        }
     }
 
+}
+
+extension PagingTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movieImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pagingImageCollectionCell", for: indexPath) as! PagingImageCollectionViewCell
+        let imagePath = "https://image.tmdb.org/t/p/w500\(movieImages[indexPath.row].file_path)"
+        print("____________", imagePath)
+        cell.pagingImageView.kf.setImage(with: URL(string: imagePath))
+        return cell
+    }
+}
+
+extension PagingTableViewCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.safeAreaLayoutGuide.layoutFrame.width
+        let height = collectionView.safeAreaLayoutGuide.layoutFrame.height
+        let size = CGSize(width: width, height: height)
+        return size
+    }
+}
+
+extension PagingTableViewCell: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) { // 컬렉션뷰를 스크롤하면 반복적으로 호출
+        let width = scrollView.bounds.size.width // 너비 저장
+        let x = scrollView.contentOffset.x + (width / 2.0) // 현재 스크롤한 x좌표 저장
+        
+        let newPage = Int(x / width)
+        if pageControl.currentPage != newPage {
+            pageControl.currentPage = newPage
+        }
+    }
 }
--- kakaoMakers/UI/DetailView/DetailViewController.swift
+++ kakaoMakers/UI/DetailView/DetailViewController.swift
@@ -7,17 +7,34 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+//protocol movieIndexProtocol {
+//    var protocolMovieIndex: Int { get set }
+//}
+//
+//struct MovieIndexSelection: movieIndexProtocol {
+//    //var movieIndex: Int
+//    var protocolMovieIndex: Int {
+//        get {
+//            return self.protocolMovieIndex
+//        }
+//        set {
+//            self.protocolMovieIndex = newValue
+//        }
+//    }
+//}
 
+class DetailViewController: UIViewController {
+    
     @IBOutlet weak var detailTableView: UITableView!
     var movieId: Int = 0
     var networkProvider = MovieNetworkManager()
     var detailMove: DetailMovie?
     var movieImages = [MovieImage]()
-        
+    var movieProtocol = MovieIndexSelection()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         detailTableView.delegate = self
         detailTableView.dataSource = self
         
@@ -25,10 +42,10 @@ class DetailViewController: UIViewController {
         detailTableView.estimatedRowHeight = 500
         
         getDetailData()
-        getMovieImages()
         navigationSetup()
-    }
         
+    }
+    
     lazy var backButton: UIBarButtonItem = {
         let buttonIcon = UIImage(systemName: "chevron.backward")
         let button = UIBarButtonItem(title: nil, style: UIBarButtonItem.Style.done, target: self, action: #selector(backBtn(_:)))
@@ -39,7 +56,7 @@ class DetailViewController: UIViewController {
     
     @objc func backBtn(_ sender:UIBarButtonItem!){
         navigationController?.popViewController(animated: true)
-      }
+    }
     
     func navigationSetup() {
         
@@ -51,26 +68,21 @@ class DetailViewController: UIViewController {
     
     func getDetailData() {
         
-        networkProvider.getDetailMovie(movieID: movieId) { result in
+        networkProvider.getDetailMovie(movieID: movieId) { [self] result in
             
             self.detailMove = result
-        
+    
+//            movieProtocol.protocolMovieIndex = movieId
+//            print("___________", movieProtocol.protocolMovieIndex)
+            
             OperationQueue.main.addOperation {
                 self.detailTableView.reloadData()
             }
         }
     }
-    
-    func getMovieImages() {
-        networkProvider.getMovieImages(movieID: movieId) { results in
-            
-            self.movieImages = results
-        }
-    }
-    
 }
 
-extension DetailViewController: UITableViewDelegate, UITableViewDataSource {
+extension DetailViewController: UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 4
@@ -115,50 +127,8 @@ extension DetailViewController: UITableViewDelegate, UITableViewDataSource {
             return cell
         }
     }
-    
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if let cell = cell as? PagingTableViewCell {
-            cell.pagingCollectionView.dataSource = self
-            cell.pagingCollectionView.delegate = self
-            cell.pagingCollectionView.register(UINib(nibName: "PagingImageCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "pagingImageCollectionCell")
-            cell.pagingCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            cell.pagingCollectionView.reloadData()
-        }
-    }
 }
 
-extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return movieImages.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pagingImageCollectionCell", for: indexPath) as! PagingImageCollectionViewCell
-        let imagePath = "https://image.tmdb.org/t/p/w500\(movieImages[indexPath.row].file_path)"
-        cell.pagingImageView.kf.setImage(with: URL(string: imagePath))
-        return cell
-    }
-}
 
-extension DetailViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.safeAreaLayoutGuide.layoutFrame.width
-        let height = collectionView.safeAreaLayoutGuide.layoutFrame.height
-        let size = CGSize(width: width, height: height)
-        return size
-    }
-}
+
 
